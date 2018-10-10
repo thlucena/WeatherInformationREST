f56009fb0c477718a4497c2e311e57a16c8b0b89
Unknown <thiago_cesarm@hotmail.com>
2018-10-10 19:53:18 -0300

    Servidor funcionando versão inicial

--- src/main/java/service/JavaResource.java
+++ src/main/java/service/JavaResource.java
@@ -1,26 +1,26 @@
 package service;
 
-import javax.ejb.Stateless;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.MediaType;
+//import javax.ejb.Stateless;
+//import javax.ws.rs.GET;
+//import javax.ws.rs.Path;
+//import javax.ws.rs.Produces;
+//import javax.ws.rs.QueryParam;
+//import javax.ws.rs.core.MediaType;
 
-@Stateless
-@Path("/hello")
-public class JavaResource implements JavaInterface{
+//@Stateless
+//@Path("/hello")
+public class JavaResource { //implements JavaInterface{
 	
-	public JavaResource() {
-		
-	}
-	
-	@GET
-	@Path("/saymyname")
-	@Produces(MediaType.TEXT_PLAIN)
-	public String hello(@QueryParam("name") String name) {
-		return "Hello, " + name + "!";
-	}
+//	public JavaResource() {
+//		
+//	}
+//	
+//	@GET
+//	@Path("/saymyname")
+//	@Produces(MediaType.TEXT_PLAIN)
+//	public String hello(@QueryParam("name") String name) {
+//		return "Hello, " + name + "!";
+//	}
 	
 //	@GET
 //	@Path("/celsiusToFahrenheit")
